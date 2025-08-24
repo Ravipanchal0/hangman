5cad21edceacedb89810ec057d6a1a59df4a371c
Ravi Panchal <ravipanchal9977@gmail.com>
2025-08-24 09:28:06 +0530

    Remove console log for fetched word in fetchGameWord function

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ const App = () => {
       const res = await fetch("https://random-word-api.herokuapp.com/word");
       const data = await res.json();
       const word = data[0];
-      console.log(word);
 
       const defRes = await fetch(
         `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
